e976eb014fe688356d81b4cd33021549e974a33f
Izabelly <izabelly.4940@gmail.com>
2024-06-27 16:36:45 -0300

    atividade 4

--- Atividades.py
+++ Atividades.py
@@ -22,3 +22,9 @@ print(cidade)
 cidade = "Rio de Janeiro"
 print(cidade)
 
+#atividade 4
+
+string = "oio"
+inteiro = 254
+float = 20.5
+print("este é uma string {}, este um inteiro {} e este uma variavel float {}".format(string, inteiro, float))
