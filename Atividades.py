72c9db169e7640d2075143510307bb26f4602792
Izabelly <izabelly.4940@gmail.com>
2024-06-27 16:32:36 -0300

    atividade2

--- Atividades.py
+++ Atividades.py
@@ -6,3 +6,10 @@ apelido = input("Qual seu apelido?")
 cidade_codigo_postal = "Curitiba, 83225456"
 
 print("{}{} mora na rua {}, cidade {}".format(nome,apelido, endereco, cidade_codigo_postal ))
+
+#atividade 2
+
+nome2= input("Digite seu nome: ")
+ano = input("Escolha um ano: ")
+#conforme é mudado o nome e ano, o texto será alterado
+print("{} é uma valente cavaleira, nascida no ano de {}. Certa manhã, {} acordou com um barulho terrível: um dragão se paroximava da aldeia. Somente {} poderia salvar os moradores da aldeia.".format(nome,ano, nome, nome))
