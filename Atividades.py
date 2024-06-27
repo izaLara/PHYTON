29c9d3127f4907958c53b43caee00a21ff44cf1f
Izabelly <izabelly.4940@gmail.com>
2024-06-27 16:35:32 -0300

    atividade 3

--- Atividades.py
+++ Atividades.py
@@ -13,3 +13,12 @@ nome2= input("Digite seu nome: ")
 ano = input("Escolha um ano: ")
 #conforme é mudado o nome e ano, o texto será alterado
 print("{} é uma valente cavaleira, nascida no ano de {}. Certa manhã, {} acordou com um barulho terrível: um dragão se paroximava da aldeia. Somente {} poderia salvar os moradores da aldeia.".format(nome,ano, nome, nome))
+
+#atividade 3 
+
+cidade = "São Paulo"
+print(cidade)
+
+cidade = "Rio de Janeiro"
+print(cidade)
+
