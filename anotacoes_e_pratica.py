86970041d13ec49c10c05e2b5d452c138de349e9
Izabelly <izabelly.4940@gmail.com>
2024-06-27 16:58:37 -0300

    anotações e pratica

--- anotacoes_e_pratica.py
+++ anotacoes_e_pratica.py
@@ -1,3 +1,29 @@
+#MEU PRIMEIRO PHYTON
+
+print("Olá mundo")
+print("Primeiro estamos praticando o comando Print")
+print("Esse comando printa 3 linhas de texto")
+
+print("Izabelly")
+print("Minha casa")
+print(19)
+
+#variavel e constante 'nome' pode ser fixa ou variar
+nome = input ("Digite seu nome:")
+print("Olá" + nome)
+
+#string f 
+
+resultado = 25 
+resultado2 = 30
+print(f"oresultado da conta é: {resultado}")
+#ou
+print("o resultado da conta é:{}".format(resultado))
+
+print(f"o resultado dos dois valores é: {resultado}, e a conta de dois valores é {resultado2}")
+print("o resultado dos dois valores é: {}, e a conta de dois valores é {}".format(resultado, resultado2))
+
+
 #PARA FAZER A CONVERSÃO
 
 # convertToInteiro = int('nome da variavel')
@@ -20,8 +46,8 @@ print(f"O IMC é: {imc}")
 
 #pratica 3
 
-nome = "Izabelly"
-print(f"Meu nome é: {nome}")
+nome2 = "Izabelly"
+print(f"Meu nome é: {nome2}")
 
 #pratica 4
 
